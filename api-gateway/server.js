91141340d90faf0a0ab8efc8a482f05e5d307e12
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2023-07-19 10:21:13 +0200

    feat(docker): :sparkles: docker added

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -4,7 +4,7 @@ const proxy = require("express-http-proxy")
 require('dotenv').config()
 const port = process.env.PORT
 
-app.use("/api/auth", proxy("http://localhost:" + process.env.AUTH_PORT))
+app.use("/api/auth", proxy("http://auth:" + process.env.AUTH_PORT))
 
 app.listen(port, () => {
   console.log('API Gateway en cours d\'exécution sur le port', port);
