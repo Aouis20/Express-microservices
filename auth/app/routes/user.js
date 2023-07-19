ea7653d3afe43be6679200afedc15942ddfe52c3
Aouis CHOUKRI <78351892+Aouis20@users.noreply.github.com>
2023-07-19 10:23:03 +0200

    refactor(routes): :fire: user.js route deleted

--- auth/app/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userCtrl = require("../controllers/user.js");
-
-router.post("/register", userCtrl.register);
-router.post('/login', userCtrl.login);
-
-module.exports = router;
